2ea9787030c2c09210a8d0a72e4fda3edf046afd
Lucas Willett <lucas@developingego.com>
2011-03-30 09:18:20 +1000

    Added test_data spec helper to retrieve test data by filename.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,8 @@ require 'damper'
 require 'rack/test'
 
 set :environment, :test
+
+def test_data(name)
+  filename = File.join(File.dirname(__FILE__), 'support', 'data', "#{name}.json")
+  Payload.new(JSON.parse(File.read(filename)))
+end
