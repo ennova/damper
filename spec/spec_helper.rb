67608b9ded67d4c42eb5c59c478fb895af9984ff
Jason Weathered <jason@jasoncodes.com>
2011-03-30 21:45:34 +1000

    Separate `payload_data` from `payload` test helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,11 @@ require 'rack/test'
 
 set :environment, :test
 
-def payload(name)
+def payload_data(name)
   filename = File.join(File.dirname(__FILE__), 'support', 'data', "#{name}.json")
-  Payload.new(JSON.parse(File.read(filename)))
+  File.read filename
+end
+
+def payload(name)
+  Payload.new JSON.parse(payload_data(name))
 end
